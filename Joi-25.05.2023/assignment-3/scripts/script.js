eb56c0ba014e56c0e382ed40b11fa7707b78af08
Ana-Delia Manoliu - Practice <anma1209@createq.space>
2023-05-25 17:50:59 +0300

    25.05.2023

--- Joi-25.05.2023/assignment-3/scripts/script.js
+++ Joi-25.05.2023/assignment-3/scripts/script.js
@@ -1,25 +1,29 @@
 const select = document.querySelector('select');
 select.addEventListener('change', () => {
       const id = select.value;
-      console.log(id);
+    //   console.log(id);
     try {
         const request = new XMLHttpRequest();
         // response handler
         request.onreadystatechange = () => {
+                
                 if (request.readyState === XMLHttpRequest.DONE) {
                     if (request.status === 200) {
-                        console.log("Response!");
+                           const response = JSON.parse(request.response);
+                           const element =  document.createElement('div');
+                           const title = document.createTextNode(response.title);
+                           element.appendChild(title);
+                           document.body.appendChild(element);
+                        
                     } else {
                         console.log("Error! Didn't get the response!");
                     }
-                } else {
-                    console.log("Error at request");
                 }
         }
         // make server request
-        request.open("POST", 'https://jsonplaceholder.typicode.com/todos/' + id, true);
+        request.open("GET", 'https://jsonplaceholder.typicode.com/todos/' + id, true);
         request.send();
     } catch (error) {
-        console.log(error);
+        // console.log(error);
     }
 })
