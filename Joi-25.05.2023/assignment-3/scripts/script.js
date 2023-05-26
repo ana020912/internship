f41e70b67c243437b7276cf8dfa18bf2f2cc55d1
ana020912 <100303079+ana020912@users.noreply.github.com>
2023-05-26 12:05:21 +0300

    Update script.js

--- Joi-25.05.2023/assignment-3/scripts/script.js
+++ Joi-25.05.2023/assignment-3/scripts/script.js
@@ -1,4 +1,6 @@
 const select = document.querySelector('select');
+const element =  document.createElement('p');
+document.body.appendChild(element);
 select.addEventListener('change', () => {
       const id = select.value;
     //   console.log(id);
@@ -10,20 +12,18 @@ select.addEventListener('change', () => {
                 if (request.readyState === XMLHttpRequest.DONE) {
                     if (request.status === 200) {
                            const response = JSON.parse(request.response);
-                           const element =  document.createElement('div');
+                           element.innerHTML = "";
                            const title = document.createTextNode(response.title);
                            element.appendChild(title);
-                           document.body.appendChild(element);
-                        
                     } else {
                         console.log("Error! Didn't get the response!");
                     }
                 }
-        }
+            } 
         // make server request
         request.open("GET", 'https://jsonplaceholder.typicode.com/todos/' + id, true);
         request.send();
     } catch (error) {
-        // console.log(error);
+        console.log(error);
     }
 })
